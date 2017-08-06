2ca2510679c651b521c629096fe37454cf14ae35
zale <zale.felin@gmail.com>
2017-08-06 13:59:58 +0800

    icri

--- CardDrawing/src/Main/Main.java
+++ CardDrawing/src/Main/Main.java
@@ -6,6 +6,7 @@
 package Main;
 
 import java.util.Scanner;
+import org.rosuda.REngine.Rserve.RConnection;
 
 /**
  *
@@ -14,19 +15,46 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int choice;
-        
+        int Cchoice, Tchoice;
+        String rbinom = "rbinom(5,10,1/6)";
         System.out.println("Card Drawing Simulation\n");
         
-        do {
-            System.out.print("Select number of cards to be drawn (1 to 5): ");
-            choice = sc.nextInt();
+        try{
+
+            System.out.println("INFO: Trying to connect to R ");
+            RConnection c = new RConnection();
+            System.out.println("INFO: Connected to R ");
+            System.out.println("INFO: The Server version is " + c.getServerVersion() + "\n");
+
+            do {
+                System.out.print("Select number of cards to be drawn (1 to 5): ");
+                Cchoice = sc.nextInt();
+
+                if (Cchoice < 1 || Cchoice > 5) {
+                    System.out.println("Please select only from 1 to 5.");
+                }
+            } while (Cchoice < 1 || Cchoice > 5);
             
-            if (choice < 1 || choice > 5) {
-                System.out.println("Please select only from 1 to 5.");
-            }
-        } while (choice < 1 || choice > 5);
-        
-        
+            do {
+                System.out.print("Select number of trials to be done (1 to 5): ");
+                Tchoice = sc.nextInt();
+
+                if (choice < 1 || choice > 5) {
+                    System.out.println("Please select only from 1 to 5.");
+                }
+            } while (choice < 1 || choice > 5);
+            
+            c.eval("result="+rbinom);
+            int[] res = c.eval("result").asIntegers();
+            for (int i = 0; i < res.length; i++)
+                System.out.println(res[i]);
+            
+            c.close();
+
+        } catch (Exception e) {
+            System.out.println("ERROR: In Connection to R ");
+            System.out.println("The Exception is " + e.getMessage());
+            e.printStackTrace();
+        }       
     }
 }
